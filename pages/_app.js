a156edff15bda1ff23e337ede099521ae9c7c998
aly8i <ABY00003@students.aust.edu.lb>
2022-10-30 00:23:56 +0200

    18

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
-import Layout from "../components/Layout";
-import "../styles/globals.css";
-import store from "../components/redux/store";
-import { Provider } from "react-redux";
-import { StyledEngineProvider } from '@mui/material/styles';
-
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-
-  return (
-    <StyledEngineProvider injectFirst>
-        <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Provider>
-    </StyledEngineProvider>
-    
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
