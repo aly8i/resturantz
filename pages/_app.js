e0e84ff0d891d2509c3e5de2779924f9497f455d
aly8i <ABY00003@students.aust.edu.lb>
2022-10-30 01:12:36 +0200

    31

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import Layout from "../components/Layout";
 import store from "../components/redux/store";
 import { Provider } from "react-redux";
-import Footer from "./Footer";
+import Footer from "../components/Footer";
 // import { StyledEngineProvider } from '@mui/material/styles';
 
 function MyApp({ Component, pageProps }) {
