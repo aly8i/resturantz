f79884327c97907bda4b3f8cd5c953906cea7ef2
aly8i <ABY00003@students.aust.edu.lb>
2022-10-30 00:38:52 +0200

    21

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-import store from "../components/redux/store";
+// import store from "../components/redux/store";
 import { Provider } from "react-redux";
 import { StyledEngineProvider } from '@mui/material/styles';
 
