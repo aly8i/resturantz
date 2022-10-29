283e3ddee20c59914561fb430383e8f2a9a38bde
aly8i <ABY00003@students.aust.edu.lb>
2022-10-29 19:26:09 +0300

    second commit

---
+++ pages/products.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import axios from 'axios'
+
+
+const products = ({products}) => {
+  return (
+    <div>{products}</div>
+  )
+}
+
+export default products
+
+export const getServerSideProps = async () => {
+    
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`).catch((err)=>{
+        console.log(err);
+    });
+
+    return {
+        props: {
+            products: res.json()
+        },
+      };
+}
