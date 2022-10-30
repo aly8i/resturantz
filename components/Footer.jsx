c4949f0e24120e17045504993d397a8fab4f785b
aly8i <ABY00003@students.aust.edu.lb>
2022-10-30 12:29:50 +0200

    38

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,15 +5,16 @@ import axios from "axios";
 import { useEffect,useState } from "react";
 const Footer = () => {
   const [sliderData,setSliderData]=useState(["https://firebasestorage.googleapis.com/v0/b/hfc-resto.appspot.com/o/pizzas%2Fresturant2.jpg?alt=media&token=c69732b4-fdfa-4ec8-b658-ff390f2fa510"]);
-  // useEffect(()=>{
-  //   try{
-  //   axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/static/`).then((res)=>{
-  //     setSliderData(res.data.slider2);
-  //   });
-  //   }catch(err){
-  //     console.log(err);
-  //   }
-  // },[])
+  useEffect(()=>{
+    try{
+    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/static/`).then((res)=>{
+      setSliderData(res.data.slider2);
+      console.log(sliderData)
+    });
+    }catch(err){
+      console.log(err);
+    }
+  },[])
   return (
     <div id="footer" className={styles.container}>
       <div className={styles.item}>
