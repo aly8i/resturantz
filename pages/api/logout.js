415e7d04a98dc0d499b8d08489ee56372087d9c2
aly8i <ABY00003@students.aust.edu.lb>
2022-11-19 22:23:23 +0200

    cookies

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,12 +1,14 @@
 import cookie from "cookie";
+import { deleteCookie } from "cookies-next";
 const handler = async(req, res) => {
-    await res.setHeader("Set-Cookie",cookie.serialize("accessToken", "", {
+    res.setHeader("Set-Cookie",cookie.serialize("accessToken", "", {
         maxAge: -1,
         sameSite: "strict",
         httpOnly: true,
         path: "/",
       })
     );
+    deleteCookie('acccessToken',{req,res});
     res.status(200).json("logged out");
 }
 export default handler;
